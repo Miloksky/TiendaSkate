17a22ca42a6e14ff8503a3a31577f306cece26b2
Camilo Coral <124797907+Miloksky@users.noreply.github.com>
2025-03-24 17:47:19 +0100

    header

---
+++ main.js
@@ -0,0 +1,19 @@
+const productos = [
+    { id: 1, nombre: "Diablo", imagen: "/SimuladorTienda/TiendaSkate/local/img1", descripcion: "Vertion Diablo", precio: 10, stock: 20 },
+    { id: 2, nombre: "Seta", imagen: "/SimuladorTienda/TiendaSkate/local/img2", descripcion: "Vertion Seta", precio: 12, stock: 15 },
+    { id: 3, nombre: "Caballero", imagen: "/SimuladorTienda/TiendaSkate/local/img3", descripcion: "Vertion Caballero", precio: 14, stock: 18 },
+    { id: 4, nombre: "Lápiz", imagen: "/SimuladorTienda/TiendaSkate/local/img4", descripcion: "Vertion Lápiz", precio: 11, stock: 22 },
+    { id: 5, nombre: "Logo Element", imagen: "/SimuladorTienda/TiendaSkate/local/img5", descripcion: "Vertion Logo Element", precio: 13, stock: 16 },
+    { id: 6, nombre: "Caballero 2", imagen: "/SimuladorTienda/TiendaSkate/local/img6", descripcion: "Vertion Caballero 2", precio: 15, stock: 19 },
+    { id: 7, nombre: "Biología", imagen: "/SimuladorTienda/TiendaSkate/local/img7", descripcion: "Vertion Biología", precio: 10, stock: 20 },
+    { id: 8, nombre: "Mariposa", imagen: "/SimuladorTienda/TiendaSkate/local/img8", descripcion: "Vertion Mariposa", precio: 12, stock: 18 },
+    { id: 9, nombre: "Mago", imagen: "/SimuladorTienda/TiendaSkate/local/img9", descripcion: "Vertion Mago", precio: 14, stock: 17 },
+    { id: 10, nombre: "Paisaje Montaña", imagen: "/SimuladorTienda/TiendaSkate/local/img10", descripcion: "Vertion Paisaje Montaña", precio: 13, stock: 21 },
+    { id: 11, nombre: "Element Classic", imagen: "/SimuladorTienda/TiendaSkate/local/img11", descripcion: "Vertion Element Classic", precio: 16, stock: 14 },
+    { id: 12, nombre: "Element New", imagen: "/SimuladorTienda/TiendaSkate/local/img12", descripcion: "Vertion Element New", precio: 14, stock: 20 },
+    { id: 13, nombre: "Psicodélico", imagen: "/SimuladorTienda/TiendaSkate/local/img13", descripcion: "Vertion Psicodélico", precio: 15, stock: 15 },
+    { id: 14, nombre: "Minimal", imagen: "/SimuladorTienda/TiendaSkate/local/img14", descripcion: "Vertion Minimal", precio: 12, stock: 19 },
+    { id: 15, nombre: "Classic", imagen: "/SimuladorTienda/TiendaSkate/local/img15", descripcion: "Vertion Classic", precio: 11, stock: 22 },
+    { id: 16, nombre: "Flowers", imagen: "/SimuladorTienda/TiendaSkate/local/img16", descripcion: "Vertion Flowers", precio: 13, stock: 18 }
+  ];
+
